4a1361612d2673b5781b205aaa0051d97f764858
Diana Verevkina <diana_verevkina@mail.ru>
2023-07-30 21:01:06 +0300

    update function * [Symbol.iterator]

--- src/__tests__/team.test.js
+++ src/__tests__/team.test.js
@@ -1,7 +1,49 @@
 import Team from '../js/Team';
+import Character from '../js/Character';
+
+test('Adding characters to set - error(character has already added)', () => {
+  const team = new Team();
+  const hero = new Character('Ivan', 'Bowman');
+  team.add(hero);
+  expect(() => {
+    team.add(hero);
+  }).toThrow('Игрок уже добавлен');
+});
+
+test('Create array from set - works', () => {
+  const team = new Team();
+  team.add(new Character('Ivan', 'Bowman'));
+  team.add(new Character('Igor', 'Undead'));
+  const expected = [
+    {
+      name: 'Ivan',
+      type: 'Bowman',
+      health: 100,
+      level: 1,
+      attack: 40,
+      defence: 10,
+    },
+    {
+      name: 'Igor',
+      type: 'Undead',
+      health: 100,
+      level: 1,
+      attack: 40,
+      defence: 10,
+    },
+  ];
+  expect(team.toArray()).toEqual(expected);
+});
 
 test('check generation of class Team', () => {
-  const generator = new Team()[Symbol.iterator]();
+  const team = new Team();
+  const hero1 = new Character('Ivan', 'Bowman');
+  const hero2 = new Character('Igor', 'Undead');
+  const hero3 = new Character('Mor', 'Magician');
+
+  team.addAll(hero1, hero2, hero3);
+
+  const generator = team[Symbol.iterator]();
   const listOfChars = [...generator];
   const expected = [
     {
@@ -13,7 +55,7 @@ test('check generation of class Team', () => {
       defence: 10,
     },
     {
-      name: 'Eva',
+      name: 'Igor',
       type: 'Undead',
       health: 100,
       level: 1,
@@ -23,14 +65,6 @@ test('check generation of class Team', () => {
     {
       name: 'Mor',
       type: 'Magician',
-      health: 100,
-      level: 1,
-      attack: 40,
-      defence: 10,
-    },
-    {
-      name: 'Klon',
-      type: 'Zombie',
       health: 100,
       level: 1,
       attack: 40,
--- src/js/Team.js
+++ src/js/Team.js
@@ -1,10 +1,29 @@
-import Character from './Character';
-
 export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Игрок уже добавлен');
+    }
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => {
+      this.members.add(character);
+    });
+  }
+
+  toArray() {
+    return Array.from(this.members);
+  }
+
   * [Symbol.iterator]() {
-    yield new Character('Ivan', 'Bowman');
-    yield new Character('Eva', 'Undead');
-    yield new Character('Mor', 'Magician');
-    yield new Character('Klon', 'Zombie');
+    const team = this.toArray();
+    for (let i = 0; i < team.length; i++) {
+      yield team[i];
+    }
   }
 }
